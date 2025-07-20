9127befff25a7f0c5d356dc4a54c943fed4d7e5f
EugeneRedwing8 <emersonejohnson@gmail.com>
2025-07-20 16:08:23 -0700

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,229 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile20 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile21 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile22 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile24 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile25 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile26 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile27 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile28 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile23 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile29 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile30 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile31 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile32 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile33 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile34 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile35 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile36 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile37 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile38 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile39 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile40 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile41 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile42 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile43 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile44 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile45 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile46 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile47 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`14000f00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . 
+`, [myTiles.transparency8], TileScale.Eight);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "transparency8":return transparency8;
+            case "TundraBush":
+            case "tile8":return tile8;
+            case "TundraGround":
+            case "tile9":return tile9;
+            case "TundraCliffTop":
+            case "tile10":return tile10;
+            case "TundraCliff":
+            case "tile11":return tile11;
+            case "TundraRock":
+            case "tile12":return tile12;
+            case "TundraClump":
+            case "tile13":return tile13;
+            case "FrozenWater":
+            case "tile14":return tile14;
+            case "PlainsBush":
+            case "tile1":return tile1;
+            case "PlainsGround":
+            case "tile2":return tile2;
+            case "PlainsCliffTop":
+            case "tile3":return tile3;
+            case "PlainsCliff":
+            case "tile4":return tile4;
+            case "PlainsRock":
+            case "tile5":return tile5;
+            case "PlainsClump":
+            case "tile6":return tile6;
+            case "BasicWater":
+            case "tile7":return tile7;
+            case "CaveBush":
+            case "tile15":return tile15;
+            case "RockyCliffTop":
+            case "tile17":return tile17;
+            case "RockyCliff":
+            case "tile18":return tile18;
+            case "CaveRock":
+            case "tile19":return tile19;
+            case "CaveClump":
+            case "tile20":return tile20;
+            case "MurkyWater":
+            case "tile21":return tile21;
+            case "BeachBush":
+            case "tile22":return tile22;
+            case "SandyCliffTop0":
+            case "tile24":return tile24;
+            case "SandyCliff":
+            case "tile25":return tile25;
+            case "BeachRock":
+            case "tile26":return tile26;
+            case "BeachClump":
+            case "tile27":return tile27;
+            case "Lava":
+            case "tile28":return tile28;
+            case "SandyGround":
+            case "tile23":return tile23;
+            case "RockyGround":
+            case "tile16":return tile16;
+            case "MountainBush":
+            case "tile29":return tile29;
+            case "MountainRock":
+            case "tile30":return tile30;
+            case "MountainClump":
+            case "tile31":return tile31;
+            case "DesertBush":
+            case "tile32":return tile32;
+            case "DesertRock":
+            case "tile33":return tile33;
+            case "DesertClump":
+            case "tile34":return tile34;
+            case "MarshBush":
+            case "tile35":return tile35;
+            case "MarshGround":
+            case "tile36":return tile36;
+            case "MarshCliffTop0":
+            case "tile37":return tile37;
+            case "MarshCliff":
+            case "tile38":return tile38;
+            case "MarshRock":
+            case "tile39":return tile39;
+            case "MarshClump":
+            case "tile40":return tile40;
+            case "Mud":
+            case "tile41":return tile41;
+            case "RuinsGround":
+            case "tile42":return tile42;
+            case "RuinsRock":
+            case "tile43":return tile43;
+            case "RuinsClump":
+            case "tile44":return tile44;
+            case "PlainsBush0":
+            case "tile45":return tile45;
+            case "PlainsGround0":
+            case "tile46":return tile46;
+            case "PlainsGround1":
+            case "tile47":return tile47;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
